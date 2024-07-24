ce7b4fbe94bd7445c47736e40de191501fa90bb0
Jefferson Ruino <123637761+JRuino@users.noreply.github.com>
2024-07-24 17:16:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,19 @@ function validateForm() {
     var password = document.getElementById('password').value;
 
     if (username === "usuario" && password === "senha123") {
-        alert("Login bem-sucedido!");
-        return true;
+        showCustomAlert();
+        return false;
     } else {
         var errorMessage = document.getElementById('errorMessage');
         errorMessage.textContent = "Usuário ou senha incorretos.";
         return false;
     }
+}
+
+function showCustomAlert() {
+    var alertBox = document.getElementById('customAlert');
+    alertBox.style.display = 'block';
+    setTimeout(function() {
+        window.location.href = "logado.html";
+    }, 3000);
 }
